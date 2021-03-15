a1dca5a95a7b0b08d2a19710fd0532e0afbd1923
clinto92 <clinto92@gmail.com>
2021-03-15 18:06:26 +0530

    .get .post .delete method implemented

--- app.js
+++ app.js
@@ -14,17 +14,54 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/wikiDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/wikiDB", { 
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+
 const articleSchema = {
   title: String,
   content: String
 };
 const Article = mongoose.model("Article", articleSchema);
 
-app.get('/', function (req, res) {
-    res.send('hello world')
+app.get('/articles', function (req, res) {
+    Article.find(function(err, foundArticles){
+      // console.log(foundArticles);
+      if (!err) {
+        res.send(foundArticles);
+      } else {
+        res.send(err);
+      }
+      
+    });
   });
 
+app.post('/articles', function(req, res){
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.send('Successfully added a new article.');
+    } else {
+      res.send(err);
+    }
+  });
+
+});
+
+app.delete('/articles', function(req,res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
 //   You can create chainable route handlers for a route path by using app.route(). Because the path is specified at a single location, creating modular routes is helpful, as is reducing redundancy and typos.
   app.route('/book')
   .get(function (req, res) {
